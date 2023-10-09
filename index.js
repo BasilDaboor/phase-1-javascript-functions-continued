676c6f41e5e0e1784a72f8b382de8db88a88d593
basil daboor <basildaboor12@gmail.com>
2023-10-09 12:13:00 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(somevalue=`roller-skate`){
+
+    return `This Saturday, I want to ${somevalue}!`
+}
+
+function mondayWork(somevalue=`go to the office`) {
+    return `This Monday, I will ${somevalue}.`
+}
+
+function wrapAdjective(symbol) {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
